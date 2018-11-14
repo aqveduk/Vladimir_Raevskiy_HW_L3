955587dd59d73e0f665396119c52c6578174767c
aqveduk <kingdon@ya.ru>
2018-11-14 19:50:31 +0300

    Task 3 begun

---
+++ Task3/Task3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Fractions
+    {
+        double num;
+        double denum;
+
+        public Fractions()
+        {
+            num = 0;
+            denum = 0;
+        }
+        public Fractions(double num, double denum)
+        {
+            this.num = num;
+            this.denum = denum;
+        }
+        public Fractions Multiply(Fractions x)
+        {
+            Fractions y = new Fractions
+            {
+
+            }
+            return y;
+        }
+            
+
+
+    }
+
+
+    class Task3
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
